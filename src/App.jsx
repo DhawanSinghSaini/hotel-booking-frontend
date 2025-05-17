e1bd288fbead6a77957c12c60c8e3a49b7e2d9ee
DhawanSinghSaini <161929806+DhawanSinghSaini@users.noreply.github.com>
2025-05-17 22:44:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from './pages/Login'; // Import the Login component
 import Signup from './pages/Signup';
 import HotelListPage from './pages/HotelListPage';
 import './components/RoomCard.module.css';
-import HotelPage from './pages/HotelPage.JSX';
+import HotelPage from './pages/HotelPage';
 import BookingPage from './pages/BookingPage';
 import Dashboard from './pages/Dashboard';
 import AboutUs from './pages/AboutUs';
